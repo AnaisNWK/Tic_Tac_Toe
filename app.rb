0045aac057f162d3945d280ce0f9eb64ef994bdb
Anais NWK <anais.nowak@gmail.com>
2020-04-23 08:10:28 +0200

    First commit

---
+++ app.rb
@@ -0,0 +1,13 @@
+# Recuperer des gems requises
+require 'bundler'
+require 'pry'
+
+Bundler.require
+
+require_relative 'lib/'
+require_relative 'lib/'
+require_relative 'lib/'
+require_relative 'lib/'
+
+ 
+ # TO DO : méthode qui initialise le jeu puis contient des boucles while pour faire tourner le jeu tant que la partie n'est pas terminée.
